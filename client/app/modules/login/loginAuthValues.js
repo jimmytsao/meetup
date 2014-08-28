22fb738c0fba746432c1e1d65ede7f1dffb1f4d1
Jimmy Tsao <jimmy.tsao@gmail.com>
2014-08-28 14:53:41 -0700

    (update) Updated oauth callback route

--- client/app/modules/login/loginAuthValues.js
+++ client/app/modules/login/loginAuthValues.js
@@ -2,14 +2,12 @@
 
 (function(){
 
-  console.log('process.env.fbCallbackNonCordova: ', process.env.fbCallbackNonCordova);
-
   angular
     .module('app.login.authValues',[])
     .value('facebookParams', {
       loginUrl: 'https://www.facebook.com/dialog/oauth',
       appId: 678308422257930,
       oauthRedirectUrl: 'http://localhost/#/auth/fb',
-      oauthRedirectUrlNonCordova: process.env.fbCallbackNonCordova || 'http://localhost:8000/oauthcallback.html'
+      oauthRedirectUrlNonCordova: 'http://jtmeetup.azurewebsites.net/oauthcallback.html'
     });
 })();
